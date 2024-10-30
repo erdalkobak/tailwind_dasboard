3c537d52c0dee385e94b68f565e95d13749aa3ea
Erdal Kobak <erdalkobak1@gmail.com>
2024-10-30 13:26:19 +0300

    sidebar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      maindark: "#022213",
+      sidetext: "#a9bfb3",
+      white: "#ffffff",
+      gray: "#808080",
+      lightgreen: "#7ead3a",
+      red: "#b63941",
+    },
     extend: {},
   },
   plugins: [],
